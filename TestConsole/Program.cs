d742d95c672e8f16d35764aeeab45561d790f0e0
rcodehub-demo <rajpwr8407@gmail.com>
2024-11-20 22:17:17 -0500

    Update Program.cs

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("This is test console app");
+Console.WriteLine("This is test console app")
 Console.ReadLine();
 
